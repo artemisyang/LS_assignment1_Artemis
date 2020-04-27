7251c78a0d4b04eee886925ba606e77f48ec55d5
artemisyang <59669569+artemisyang@users.noreply.github.com>
2020-04-27 17:08:48 -0500

    Update health_index.py

--- health_index.py
+++ health_index.py
@@ -42,11 +42,11 @@ def sim_health_index():
       z_tm1 = z_t
 
   # Gather all simulation arrays to buffer on rank 0
-  # z_mat_all = None
+   z_mat_all = None
   if rank == 0:
     # z_mat_all = np.empty([N*size, (T, N)], dtype = 'float')
     # z_mat_all = np.zeros((int(N*size), T))
-    # z_mat_all = np.empty([S, T], dtype='float')
+     z_mat_all = np.empty([S, T], dtype='float')
    #  comm.Gather(sendbuf=z_mat, recvbuf=z_mat_all, root=0)
       comm.gather(z_mat, root=0)
      
